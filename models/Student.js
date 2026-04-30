e872a5168ca5f529ad7924ce752162a2dc203c9f
roarsher <154459620+roarsher@users.noreply.github.com>
2026-04-30 11:20:42 +0530

    roll issue

--- models/Student.js
+++ models/Student.js
@@ -21,5 +21,10 @@ const studentSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+studentSchema.methods.assignNumbers = function (rollSerial, regSerial) {
+  this.rollNumber = `RN-${rollSerial}-${Date.now()}`;
+  this.registrationNumber = `REG-${regSerial}-${Date.now()}`;
+  this.year = Math.ceil(this.semester / 2);
+};
 
 module.exports = mongoose.model("Student", studentSchema);
